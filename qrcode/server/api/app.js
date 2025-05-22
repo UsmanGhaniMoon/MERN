3549c979acaa102b5fb460d908e0e02f76192df4
UsmanGhaniMoon <moong3555@gmail.com>
2025-05-22 19:47:40 +0500

    QR code backend project deployed on Vercel

--- qrcode/server/api/app.js
+++ qrcode/server/api/app.js
@@ -2,12 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const db = require('./db')
+const serverless = require('serverless-http')
+const db = require('../db')
 
 //const userModle = require('./Model/User')
 
 //All router will be here
-const userRouter = require('./Route/UserRoute')
+const userRouter = require('../Route/UserRoute')
 
 const app = express()
 
@@ -20,11 +21,13 @@ app.use(cors({
 //All route initiate here
 app.use('/api/user', userRouter)
 
+module.exports = app
+module.exports.handler = serverless(app)
 
-const port = process.env.PORT || 5000
+// const port = process.env.PORT || 5000
 
-app.listen(port, (req,res)=>{
-    console.log(`Server is running on port : http://127.0.0.1:${port}`)
-})
+// app.listen(port, (req,res)=>{
+//     console.log(`Server is running on port : http://127.0.0.1:${port}`)
+// })
 
 
